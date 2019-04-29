1c3af54a126f25a8c3406d19be4723c3ae226660
connormayfield <connormayfield@gmail.com>
2019-04-29 09:40:01 -0500

    troubleshooting

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 /* port */
-process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-};
-// var PORT = process.env.PORT || 3000;
+
+var PORT = process.env.PORT || 3000;
 
 /* setting */
 var express = require("express");
